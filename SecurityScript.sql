954fdd7725a8e3462f554d9d5c4f54f60e86dce2
vfbaldovin <virgil-florentin.baldovin@expleogroup.com>
2019-12-05 17:38:48 +0200

    matrixDropDown to be continued

---
+++ SecurityScript.sql
@@ -0,0 +1,90 @@
+DROP DATABASE  IF EXISTS `webcm_security`;
+
+CREATE DATABASE  IF NOT EXISTS `webcm_security`;
+USE `webcm_security`;
+
+--
+-- Table structure for table `users`
+-- authorities
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id_user` INT(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL UNIQUE,
+  `password` char(68) NOT NULL,
+  `enabled` tinyint(1) NOT NULL,
+  `reset_token` char(36) UNIQUE,
+  `expiryDate` char(30),
+  PRIMARY KEY (`id_user`)
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+--
+-- Dumping data for table `users`
+--
+-- NOTE: The passwords are encrypted using BCrypt
+
+-- Default passwords here are: fun123
+--
+
+INSERT INTO `users` 
+VALUES 
+(1,'ovidiu-marian.milea@expleogroup.com','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,NULL,NULL),
+(2,'tiberiu.rusu@expleogroup.com','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,NULL,NULL),
+(3,'virgil-florentin.baldovin@expleogroup.com','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,NULL,NULL),
+(4,'dumitru-cristian.sterpu@expleogroup.com','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,NULL,NULL),
+(5,'admin@expleogroup.com','{bcrypt}$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K',1,NULL,NULL);
+
+
+/* JOIN TABLE for MANY-TO-MANY relationship*/  
+DROP TABLE IF EXISTS `roles`;
+CREATE TABLE `roles` (
+    `id_roles` INT(11) NOT NULL AUTO_INCREMENT,
+    `user_roles` VARCHAR(50) NOT NULL UNIQUE,
+	PRIMARY KEY(`id_roles`)
+)ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+INSERT INTO `roles` 
+VALUES 
+(1,'ROLE_EMPLOYEE'),
+(2,'ROLE_MANAGER'),
+(3,'ROLE_ADMIN');
+
+--
+-- Table structure for table `authorities`
+--
+
+DROP TABLE IF EXISTS `authorities`;
+CREATE TABLE `authorities` (
+  `username` int(11) NOT NULL,
+  `authority` int(11) NOT NULL,
+  
+  PRIMARY KEY (`username`,`authority`),
+  KEY `authority` (`authority`),
+  
+  CONSTRAINT `authorities_ibfk_1` FOREIGN KEY (`username`) REFERENCES `users` (`id_user`),
+  CONSTRAINT `authorities_ibfk_2` FOREIGN KEY (`authority`) REFERENCES `roles` (`id_roles`)
+
+  
+) ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+SET FOREIGN_KEY_CHECKS = 1;
+
+--
+-- Dumping data for table `authorities`
+--
+
+INSERT INTO `authorities` 
+VALUES 
+(1,1),
+(2,1),
+(2,2),
+(3,1),
+(4,1),
+(4,2),
+(5,1),
+(5,3);
---
+++ expleodb.sql
@@ -0,0 +1,156 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema expleodb
+-- -----------------------------------------------------
+DROP DATABASE IF EXISTS `expleodb` ;
+CREATE DATABASE IF NOT EXISTS `expleodb` DEFAULT CHARACTER SET utf8 COLLATE utf8_romanian_ci ;
+USE `expleodb` ;
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`proiect`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`proiect` (
+  `ID_Proiect` INT(11) NOT NULL AUTO_INCREMENT,
+  `Nume_proiect` VARCHAR(45) NOT NULL,
+  `Cod` VARCHAR(15) NOT NULL UNIQUE,
+  `Manager_email` VARCHAR(60),
+  PRIMARY KEY (`ID_Proiect`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+INSERT INTO `proiect` VALUES 
+	(1,'webCM','I1030', null),
+	(2,'Plastice','P1056', null),
+	(3,'Motoare','P1020', null);
+
+
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`skill`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`skill` (
+  `ID_skill` INT(11) NOT NULL AUTO_INCREMENT,
+  `Nume_skill` VARCHAR(45) NOT NULL,
+  `Categorie` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`ID_skill`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 2
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+INSERT INTO `skill` VALUES 
+	(1,'Java','Programare'),
+	(2,'C','Programare'),
+	(3,'C++','Programare'),
+	(4,'Python','Programare'),
+	(5,'Catia V5','Proiectare'),
+	(6,'Plastice','Proiectare'),
+	(7,'Motoare','Proiectare'),
+	(8,'Drawings','Proiectare'),
+	(9,'Motivare','Management'),
+	(10,'Luarea deciziilor','Management');
+
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`proiect_skill`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`proiect_skill` (
+  `ID_skill` INT(11) NOT NULL,
+  `ID_proiect` INT(11) NOT NULL,
+  `Pondere` INT(2) UNSIGNED NOT NULL DEFAULT 1,
+  `Target` INT(1) UNSIGNED NOT NULL DEFAULT 1,
+  INDEX `FK_proiect_skill_skill` (`ID_skill` ASC) VISIBLE,
+  INDEX `FK_proiect_skill_proiect` (`ID_proiect` ASC) VISIBLE,
+  CONSTRAINT `FK_proiect_skill_proiect`
+    FOREIGN KEY (`ID_proiect`)
+    REFERENCES `expleodb`.`proiect` (`ID_Proiect`),
+  CONSTRAINT `FK_proiect_skill_skill`
+    FOREIGN KEY (`ID_skill`)
+    REFERENCES `expleodb`.`skill` (`ID_skill`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`user`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`user` (
+  `ID_user` INT(11) NOT NULL AUTO_INCREMENT,
+  `Nume_user` VARCHAR(20) NOT NULL,
+  `Prenume_user` VARCHAR(40) NOT NULL,
+  `Numar_matricol` INT(3) NOT NULL UNIQUE,
+  `Email` VARCHAR(60) NOT NULL UNIQUE,
+  `Data_angajare` DATE NOT NULL,
+  `Functie` VARCHAR(20) NOT NULL,
+  PRIMARY KEY (`ID_user`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+INSERT INTO `user` VALUES 
+	(1,'Milea','Ovidiu','3465','ovidiu-marian.milea@expleogroup.com','2017-11-20','Angajat'),
+	(2,'Rusu','Tiberiu','5421','tiberiu.rusu@expleogroup.com','2019-07-20','Angajat'),
+	(3,'Baldovin','Virgil-Florentin','5428','virgil-florentin.baldovin@expleogroup.com','2019-07-26','Angajat'),
+	(4,'Sterpu','Cristian','1823','dumitru-cristian.sterpu@expleogroup.com','2010-02-13','LT'),
+	(5,'Admin','Admin','0000','admin@expleogroup.com','2019-11-18','Admin');
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`user_proiect`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`user_proiect` (
+  `ID_user` INT(11) NOT NULL,
+  `ID_proiect` INT(11) NOT NULL,
+  INDEX `FK_user_proiect_proiect` (`ID_proiect` ASC) VISIBLE,
+  INDEX `FK_user_proiect_user` (`ID_user` ASC) VISIBLE,
+  CONSTRAINT `FK_user_proiect_proiect`
+    FOREIGN KEY (`ID_proiect`)
+    REFERENCES `expleodb`.`proiect` (`ID_Proiect`),
+  CONSTRAINT `FK_user_proiect_user`
+    FOREIGN KEY (`ID_user`)
+    REFERENCES `expleodb`.`user` (`ID_user`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+
+-- -----------------------------------------------------
+-- Table `expleodb`.`user_skill`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `expleodb`.`user_skill` (
+  `ID_user_skill` INT(11) NOT NULL AUTO_INCREMENT,
+  `ID_user` INT(11) NOT NULL,
+  `ID_skill` INT(11) NOT NULL,
+  `Evaluare` INT(1) UNSIGNED NOT NULL DEFAULT 1,
+  INDEX `FK_user_skill_user` (`ID_user` ASC) VISIBLE,
+  INDEX `FK_user_skill_skill` (`ID_skill` ASC) VISIBLE,
+  CONSTRAINT `FK_user_skill_skill`
+    FOREIGN KEY (`ID_skill`)
+    REFERENCES `expleodb`.`skill` (`ID_skill`),
+  CONSTRAINT `FK_user_skill_user`
+    FOREIGN KEY (`ID_user`)
+    REFERENCES `expleodb`.`user` (`ID_user`),
+    PRIMARY KEY (`ID_user_skill`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+CREATE TABLE IF NOT EXISTS `expleodb`.`history` (
+  `ID_history` INT(11) NOT NULL AUTO_INCREMENT,
+  `ID_user_skill` INT(11) NOT NULL AUTO_INCREMENT,
+  `Evaluare` INT(1) UNSIGNED NOT NULL DEFAULT 1,
+  `Data_evaluare` DATE NOT NULL,
+  PRIMARY KEY (`ID_history`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 4
+DEFAULT CHARACTER SET = utf8
+COLLATE = utf8_romanian_ci;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
