205c872cea665fbae9344a9e3192bbb19a81448f
Baldo <virgilbaldovin6@gmail.com>
2019-12-13 00:11:21 +0200

    PDF done

--- src/main/java/com/expleo/webcm/helper/CreatePdf.java
+++ src/main/java/com/expleo/webcm/helper/CreatePdf.java
@@ -3,15 +3,23 @@ package com.expleo.webcm.helper;
 import com.expleo.webcm.entity.expleodb.UserSkill;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
-import com.itextpdf.kernel.colors.DeviceRgb;
 
+import com.itextpdf.kernel.colors.DeviceRgb;
+import com.itextpdf.kernel.events.IEventHandler;
+import com.itextpdf.kernel.events.PdfDocumentEvent;
+import com.itextpdf.kernel.geom.Rectangle;
 import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
+import com.itextpdf.kernel.pdf.xobject.PdfFormXObject;
+
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.*;
 import com.itextpdf.layout.element.Image;
+import com.itextpdf.layout.property.TabAlignment;
 import com.itextpdf.layout.property.TextAlignment;
-
+import com.itextpdf.layout.Canvas;
 
 import java.awt.*;
 import java.io.ByteArrayInputStream;
@@ -27,14 +35,27 @@ public class CreatePdf {
     public ByteArrayInputStream getPdfAsByteArrayInputStream(List<UserSkill> userSkills, String text, String evaluation) throws IOException {
 
         ImageData data = ImageDataFactory.create(getClass().getClassLoader().getResource("expleoImg.png"));
-        Image image = new Image(data).scale(2,2);
+        Image image = new Image(data).scale((float)2.5, (float) 2.5);
 
         ByteArrayOutputStream os = new ByteArrayOutputStream();
         PdfWriter writer = new PdfWriter(os);
         PdfDocument pdf = new PdfDocument(writer);
         Document document = new Document(pdf);
 
-        document.setMargins(10,30,10,30);
+        DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm");
+        Date dateobj = new Date();
+        Paragraph paragraph = new Paragraph(df.format(dateobj) + "\nTermenul cautarii: " + text + "\nEvaluare minimum: " + evaluation + " \n\n");
+
+        Header headerHandler = new Header(paragraph);
+        Footer footerHandler = new Footer();
+
+        pdf.addEventHandler(PdfDocumentEvent.START_PAGE, headerHandler);
+        pdf.addEventHandler(PdfDocumentEvent.END_PAGE, footerHandler);
+
+        // Write the total number of pages to the placeholder
+        footerHandler.writeTotal(pdf);
+
+        document.setMargins(120,30,10,30);
 
         Table table = new Table(new float[6]).useAllAvailableWidth();
         table.setMarginTop(0);
@@ -67,13 +88,8 @@ public class CreatePdf {
         }
 
         try {
-            DateFormat df = new SimpleDateFormat("dd/MM/yy HH:mm");
-            Date dateobj = new Date();
-            Paragraph paragraph = new Paragraph(df.format(dateobj) + "\nTermenul cautarii: " + text + "\nEvaluare minimum: " + evaluation + " \n\n");
 
-            document.add(image);
-            document.add(paragraph);
-            document.add(table);
+            document.add(table.setMargins(0,20,60,20));
             pdf.close();
             document.close();
 
@@ -90,4 +106,76 @@ public class CreatePdf {
         cell.setTextAlignment(textAlignment);
         return cell;
     }
+
+    static protected class Header implements IEventHandler {
+        private Paragraph header;
+
+        public Header(Paragraph header) {
+            this.header = header;
+        }
+
+        @Override
+        public void handleEvent(com.itextpdf.kernel.events.Event event) {
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+            PdfDocument pdf = docEvent.getDocument();
+
+            PdfPage page = docEvent.getPage();
+            Rectangle pageSize = page.getPageSize();
+
+            Canvas canvas = new Canvas(new PdfCanvas(page),pdf,pageSize);//new PdfCanvas(page), pdf, pageSize
+
+            ImageData data = ImageDataFactory.create(getClass().getClassLoader().getResource("expleoImg.png"));
+            Image image = new Image(data).scale((float)2.8, (float) 2.8);
+
+            // Write text at positionz
+            canvas.showTextAligned(header,
+                    pageSize.getWidth() / 2 + 250,
+                    pageSize.getTop() - 80, TextAlignment.RIGHT).add(image);
+            canvas.close();
+        }
+    }
+
+    static protected class Footer implements IEventHandler {
+         PdfFormXObject placeholder;
+         float side = 20;
+         float x = 50;
+         float y = 25;
+         float space = 4.5f;
+         float descent = 3;
+
+        public Footer() {
+            placeholder = new PdfFormXObject(new Rectangle(0, 0, side, side));
+        }
+
+        public void writeTotal(PdfDocument pdf) {
+            Canvas canvas = new Canvas(placeholder, pdf);
+            canvas.showTextAligned(String.valueOf(pdf.getNumberOfPages()),
+                    0, descent, TextAlignment.LEFT);
+            canvas.close();
+        }
+
+        @Override
+        public void handleEvent(com.itextpdf.kernel.events.Event event) {
+
+            PdfDocumentEvent docEvent = (PdfDocumentEvent) event;
+            PdfDocument pdf = docEvent.getDocument();
+            PdfPage page = docEvent.getPage();
+            int pageNumber = pdf.getPageNumber(page);
+            Rectangle pageSize = page.getPageSize();
+
+            PdfCanvas pdfCanvas = new PdfCanvas(page);
+            Canvas canvas = new Canvas(pdfCanvas, pdf, pageSize);
+
+                    Paragraph p = new Paragraph().add("© expleo Romania | Internal");
+                    p.add(new Tab());
+                    p.addTabStops(new TabStop(500, TabAlignment.RIGHT));
+                    p.add(pageNumber +"/"+ pdf.getNumberOfPages());
+
+            canvas.showTextAligned(p, x, y, TextAlignment.LEFT);
+
+            canvas.close();
+
+        }
+    }
+
 }
