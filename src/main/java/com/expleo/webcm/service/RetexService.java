27f720fe3c929713569a021d0c8b7d4f8a8eb35c
ommilea <ovidiu-marian.milea@expleogroup.com>
2020-01-16 14:36:10 +0200

    get/save record/solution

--- src/main/java/com/expleo/webcm/service/RetexService.java
+++ src/main/java/com/expleo/webcm/service/RetexService.java
@@ -1,9 +1,14 @@
 package com.expleo.webcm.service;
 
 import com.expleo.webcm.entity.expleodb.Record;
+import com.expleo.webcm.entity.expleodb.Solution;
 
 import java.util.List;
 
 public interface RetexService {
     List<Record> searchRecords(String searchTerms, String searchCategory);
+    Record getRecord(Integer recordId);
+    void saveOrUpdateRecord(Record record);
+    Solution getSolution(Integer solutionId);
+    void saveOrUpdateSolution(Solution solution);
 }
