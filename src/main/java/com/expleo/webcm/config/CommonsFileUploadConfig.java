d2d098a1dd9656deaf203cc9dcdbaef95aac2e9f
dcsterpu <dcsterpu>
2021-01-06 09:37:50 +0200

    new deposit

--- src/main/java/com/expleo/webcm/config/CommonsFileUploadConfig.java
+++ src/main/java/com/expleo/webcm/config/CommonsFileUploadConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
+// configuration
 @Configuration
 public class CommonsFileUploadConfig {
     @Bean(name = "multipartResolver")
